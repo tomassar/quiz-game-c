b65e715d3eae210ec10eec77bfecc6792a7e99bc
Tomás Sarmiento <t.sarmiento01@ufromail.cl>
2024-04-06 12:44:21 -0300

    feat: add questions to quiz

--- main.c
+++ main.c
@@ -10,8 +10,8 @@ int main()
 
     printf("   Welcome to the Game \n\n");
 
-    printf("> Press 7 to start the game");
-    printf("> Press 0 to quit the game");
+    printf("> Press 7 to start the game\n");
+    printf("> Press 0 to quit the game\n");
 
     scanf("%d", &i);
 
@@ -26,5 +26,50 @@ int main()
     else
     {
         printf("Invalid\n\n");
+    }
+
+    if (i == 7)
+    {
+        printf("1) Which one is the first search engine in the internet?\n\n");
+        printf("1) Google\n");
+        printf("2) Archie\n");
+        printf("3) Duckduck go\n");
+
+        printf("Enter your answer: ");
+        scanf("%d", &ans1);
+
+        if (ans1 == 2)
+        {
+            printf("Correct answer\n");
+            point1 = 5;
+            printf("You have scored %d points\n", point1);
+        }
+        else
+        {
+            printf("Wrong answer\n");
+            point01 = 0;
+            printf("You have scored %d points\n", point01);
+        }
+
+        printf("2) What is the decimal equivalent for 4f2h?\n\n");
+        printf("1) 23");
+        printf("2) 2950");
+        printf("3) 1266");
+
+        printf("Enter your answer: ");
+        scanf("%d", &ans2);
+
+        if (ans2 == 3)
+        {
+            printf("Correct answer\n");
+            point2 = 5;
+            printf("You have scored %d points\n", point2);
+        }
+        else
+        {
+            printf("Wrong answer\n");
+            point02 = 0;
+            printf("You have scored %d points\n", point02);
+        }
     }
 }
